5a0f9846a456faf530ae975432bab83db0c4febf
彭奕茗 <99717919+0kz@users.noreply.github.com>
2024-04-18 15:57:24 +0800

    Update jhsh.js

--- jhsh.js
+++ jhsh.js
@@ -1,4 +1,12 @@
 
+ [rewrite_local]
+ # 掌上公交微信小程序去除广告（2022-03-01）
+ https:\/\/yunbusiness\.ccb\.com\/clp_coupon\/txCtrl\?txcode=A3341C113$ url script-response-body https://raw.githubusercontent.com/0kz/rewrite/master/jhsh.js
+
+ [mitm]
+ hostname=yunbusiness.ccb.com
+
+ ***********************************/
 // 替换 "SURPLUS_STOCK":"0" 为 "SURPLUS_STOCK":"30000"
 jsonData = $response.body.replace(/"SURPLUS_STOCK":"0"/g, '"SURPLUS_STOCK":"30000"').replace(/"SYSTEM_TIME":"([^"]+)"/g, '"SYSTEM_TIME":"2024-04-19 10:30:00"');
 
