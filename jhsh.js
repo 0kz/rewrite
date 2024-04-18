1756f8c8228486fa35779a6c0b4465e2b3b31765
彭奕茗 <99717919+0kz@users.noreply.github.com>
2024-04-18 15:55:16 +0800

    Update jhsh.js

--- jhsh.js
+++ jhsh.js
@@ -1,12 +1,8 @@
-const url = $request.url;
-if (!$response.body) $done({});
-let jsonData = JSON.parse($response.body);
 
 // 替换 "SURPLUS_STOCK":"0" 为 "SURPLUS_STOCK":"30000"
-jsonData = jsonData.replace(/"SURPLUS_STOCK":"0"/g, '"SURPLUS_STOCK":"30000"');
+jsonData = $response.body.replace(/"SURPLUS_STOCK":"0"/g, '"SURPLUS_STOCK":"30000"').replace(/"SYSTEM_TIME":"([^"]+)"/g, '"SYSTEM_TIME":"2024-04-19 10:30:00"');
 
 // 替换 "SYSTEM_TIME":"([^"]+)" 为 "SYSTEM_TIME":"2024-04-19 10:30:00"
-jsonData = jsonData.replace(/"SYSTEM_TIME":"([^"]+)"/g, '"SYSTEM_TIME":"2024-04-19 10:30:00"');
 
 
 $done({ body: JSON.stringify(obj) });
